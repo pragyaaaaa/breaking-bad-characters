3f2872bb5f2ddf4bc35e90412c1dfc7911d11c96
pragyaaaaa <pragyasabstracts@gmail.com>
2020-08-09 15:08:46 +0530

    --hitting base API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Header from './components/ui/Header'
 
-function App() {
+const App = () => {
+  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchCharcters = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`)
+
+      console.log(result.data)
+    }
+    fetchCharcters()
+  }, [])
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Header />
     </div>
   );
 }
