a1fc4ba270572b82c6722c54b8f4911d490cd236
pragyaaaaa <pragyasabstracts@gmail.com>
2020-08-09 15:44:06 +0530

    --spinner added

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Character from './Character'
+import Spinner from '../ui/Spinner'
 
-const CharacterGrid = ({characters, isLoading}) => {
-    return isLoading ?(<h1>Loading...</h1>):(<section className="cards">
-        {characters.map(character => (
-            <Character key={character.char_id} character={character}></Character>
-        ))}
-    </section>)
+const CharacterGrid = ({ characters, isLoading }) => {
+    return isLoading ?
+        (<Spinner></Spinner>)
+        :
+        (<section className="cards">
+            {characters.map(character => (
+                <Character key={character.char_id} character={character}></Character>
+            ))}
+        </section>)
 }
 
 export default CharacterGrid
